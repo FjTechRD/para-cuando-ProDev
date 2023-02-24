c661b661ef064b90883349b0d8d13665051f6430
FjTechRD <fjtechnologyrd@gmail.com>
2023-02-24 11:00:53 -0400

    feat: like button

--- components/EventCard.jsx
+++ components/EventCard.jsx
@@ -1,20 +1,14 @@
 const EventCard = () => {
   return (
     <article className="event__card">
-      <div className="header__card">
-        <img src="" alt="Artist Image" />
-      </div>
+      <div className="header__card"></div>
       <div className="body__card">
         <h3>Concert #1</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo fugit
-          obcaecati quos dignissimos amet impedit odit molestiae rem,{' '}
-        </p>
+        <p></p>
       </div>
       <div className="footer__card">
-        <a href="">Enlace de Destino</a>
+        <a href="">Href</a>
         <div>
-          <img src="" alt="" />
           <p>vte count</p>
         </div>
       </div>
--- components/LikeButton.jsx
+++ components/LikeButton.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 
 const LikeButton = () => {
-  return <button>Like</button>;
+  const [isLike, setIsLike] = useState(false);
+  const Icons = isLike ? (
+    <AiFillHeart className="like__icon" />
+  ) : (
+    <AiOutlineHeart className="unlike__icon" />
+  );
+  const handleClickLike = (event) => {
+    event.preventDefault();
+    if (isLike == false) {
+      setIsLike(true);
+    } else {
+      setIsLike(false);
+    }
+  };
+
+  return <button onClick={handleClickLike}>{Icons}</button>;
 };
 
 export default LikeButton;
